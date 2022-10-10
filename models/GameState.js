c8c6cd45ed47ff4c85f504a481669bc43e1b31c0
jakerinahmed <jakerin.ahmed2001@gmail.com>
2022-10-10 15:56:56 +0100

    add GameState class

---
+++ models/GameState.js
@@ -0,0 +1,22 @@
+class GameState {
+    constructor(category, difficulty, host, roomId, questions) {
+        this.roomId = roomId;
+        this.category = category;
+        this.difficulty = difficulty;
+        this.host = host
+
+        this.users = [
+            {
+                name: host,
+                score: 0,
+                hasCompletedQuiz: false
+            }
+        ];
+
+        this.questionNumber = 1;
+        this.questions = questions;
+        this.isGameStarted = false;
+    }
+}
+
+
